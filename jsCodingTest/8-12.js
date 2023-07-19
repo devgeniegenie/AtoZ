e49257323cdd24f0b448f0e40d63ec9d50c6aec5
devgeniegenie <devgeniegenie@gmail.com>
2023-07-19 12:19:41 +0900

    js - 코테(인프런) - 메모이제이션 사용코드 추가(8-12)

--- jsCodingTest/8-12.js
+++ jsCodingTest/8-12.js
@@ -1,7 +1,7 @@
-const n = 5;
-const r = 3;
+const n = 33;
+const r = 19;
 
-const mySol = (n, r) => {
+/*const mySol = (n, r) => {
     const DFS = (n, r) => {
         if (n === r || r === 0) {
             return 1;
@@ -12,4 +12,19 @@ const mySol = (n, r) => {
     console.log(DFS(n, r));
 }
 
-mySol(n, r);
+mySol(n, r);*/
+
+const mySol2 = (n, r) => {
+    const mem = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+    const DFS = (n, r) => {
+        if (mem[n][r] > 0) return mem[n][r];
+        if (n === r || r === 0) {
+            return 1;
+        } else {
+            mem[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+            return DFS(n - 1, r - 1) + DFS(n - 1, r);
+        }
+    };
+    console.log(DFS(n, r));
+};
+mySol2(n, r);
