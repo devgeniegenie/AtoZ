48fb94c079efd2a2e207d69dc5df81f79612e0e7
Genie <devgeniegenie@gmail.com>
2023-09-16 00:02:31 +0900

    js - 정렬 - selection 개선코드 작성

--- sort/selection.js
+++ sort/selection.js
@@ -1,4 +1,4 @@
-const input = [6, 3, 2, 7, 5, 9, 1];
+const input = [10, 6, 3, 2, 7, 5, 9, 1];
 
 /**
  * wiki
@@ -27,7 +27,36 @@ selection(input);
 
 /**
  * 개선방법
- * 1. 이중 선택 정렬: 한 번의 탐색에서 최솟값과 최댓값을 같이 찾는 방법이다. 탐색 횟수가 절반으로 줄어들게 된다.
- * 2. 탐색을 응용하여 개선: 한 번의 탐색 때 동일한 값이 있다면 함께 정렬하는 방법이다.
- *  즉, 만약 최솟값을 찾았는데 그 값과 같은 값이 있다면 다음 번 탐색 때 최솟값으로 탐색될 것이기에 이 값도 탐색된 것으로 보고 미리 정렬한다. 같은 값이 많을수록 유용하게 된다.
+ * 이중 선택 정렬: 한 번의 탐색에서 최솟값과 최댓값을 같이 찾는 방법이다. 탐색 횟수가 절반으로 줄어들게 된다.
  */
+
+const selection2 = arr => {
+    const length = arr.length;
+
+    for(let i = 0; i < Math.floor(length / 2); i++) {
+        let max = i, min = i;
+        for(let j = i +1; j < length - i; j++) {
+            if(arr[max] < arr[j]) {
+                max = j;
+            }
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        /*const tempMin = arr[i];
+        arr[i] = arr[min];
+        arr[min] = tempMin;*/
+
+        /*const tempMax = arr[length - i - 1];
+        arr[length - i - 1] = arr[max];
+        arr[max] = tempMax;*/
+        // [arr[length - i - 1], arr[max]] = [arr[max], arr[length - i - 1]]
+
+        [arr[i], arr[min]] = [arr[min], arr[i]]; //구조분해할당
+        [arr[length - i - 1], arr[max]] = [arr[max], arr[length - i - 1]];
+    }
+
+    console.log(arr);
+};
+
+selection2(input);
