3d83716c85e8b6d800edd95c0b6ee5e71705661c
kimcheonjae <kcj8627@naonsoft.com>
2023-12-27 17:20:47 +0900

    10815 이분탐색으로 시간제한 해결

--- baeckjoon/bookmark1/10815.js
+++ baeckjoon/bookmark1/10815.js
@@ -9,15 +9,50 @@ const [n, a, m, a2] = (process.platform === "linux"
     .trim()
     .split("\n");
 
-const arr1 = a.split(' ').map(e => Number(e));
+const arr1 = a.split(' ').map(e => Number(e)).sort((a, b) => a - b);
 const arr2 = a2.split(' ').map(e => Number(e));
 
-let result = '';
-for (let i = 0; i < arr2.length; i++) {
-    for (let j = 0; j < arr1.length; j++) {
-        if (arr2[i] == arr1[j]) {
+const mySol = (arr1, arr2) => {
+    let result = '';
+    for (let i = 0; i < arr2.length; i++) {
+        for (let j = 0; j < arr1.length; j++) {
+            if (arr2[i] == arr1[j]) {
+                result += 1 + ' ';
+            }
+        }
+        if (result.length == i * 2) result += 0 + ' ';
+    }
+    console.log(result);
+}
+
+// mySol(arr1, arr2)
+
+const mySol2 = (arr1, arr2) => {
+    let result = '';
+    for (let i = 0; i < arr2.length; i++) {
+        let check = binarySearch(arr2[i], arr1);
+        if (check) {
             result += 1 + ' ';
+        } else {
+            result += 0 + ' ';
         }
     }
-    if (result.length == i * 2) result += 0 + ' ';
+    console.log(result);
 }
+
+const binarySearch = (n, arr) => {
+    let p = 0, q = arr.length - 1;
+    while (p <= q) {
+        let x = Math.floor((p + q) / 2);
+        if (n == arr[x]) {
+            return true
+        } else if (n > arr[x]) {
+            p = x + 1;
+        } else if (n < arr[x]) {
+            q = x - 1;
+        }
+    }
+    return false;
+};
+
+mySol2(arr1, arr2)
