75a41a81d255f50046905be3e0ba9c4492f41776
kim.cheonjae <kcj8627@naonsoft.com>
2024-08-07 14:52:39 +0900

    leetcode : two-sum. https://leetcode.com/problems/two-sum/?submissionId=1347369984

---
+++ jsCodingTest/leetcode/two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/problems/two-sum/?submissionId=1347369984
+ */
+
+var twoSum = function(nums, target) {
+    const result = [];
+    for(let i = 0; i < nums.length - 1; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                result.push(i);
+                result.push(j);
+                break;
+            }
+        }
+    }
+    return result;
+};
+
+console.log(twoSum([0,4,3,0], 0))
